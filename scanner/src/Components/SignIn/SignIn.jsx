e58f1dbed56a1737b74c3ff99c467d9005ea4815
Skander-benRomdhane <bromdhaneskander@gmail.com>
2020-11-18 16:48:33 +0100

    we still have the style connect log out check qr code and delete it

--- scanner/src/Components/SignIn/SignIn.jsx
+++ scanner/src/Components/SignIn/SignIn.jsx
@@ -1,34 +1,57 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import axios from "axios";
+import { login } from "../../actions/auth";
 
 class SignIn extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       id: "",
+      password: "",
+      user: [],
+      root: "/",
     };
     this.onChangeVal = this.onChangeVal.bind(this);
+    this.checkUser = this.checkUser.bind(this);
   }
   onChangeVal = (e) => {
     this.setState({
       [e.target.name]: e.target.value,
     });
   };
+  checkUser = (e) => {
+    e.preventDefault();
+    const { id, password } = this.state;
+    this.props.login(id, password);
+  };
   render() {
-    console.log(this.state.id);
+    if (this.props.auth.isAuthenticated)
+      return <Redirect to="/scanner"></Redirect>;
+
     return (
       <div>
-        <input
-          name="id"
-          onChange={this.onChangeVal}
-          placeholder="enter your secret id"
-        ></input>
-        <button>
-          <Link to="/scanner" /> click me
-        </button>
+        <form action="" onSubmit={this.checkUser}>
+          <input
+            name="password"
+            onChange={this.onChangeVal}
+            placeholder="enter your secret id"
+          ></input>
+          <input
+            name="id"
+            onChange={this.onChangeVal}
+            placeholder="enter your secret id"
+          ></input>
+          <input type="submit" value="login" />
+        </form>
       </div>
     );
   }
 }
 
-export default SignIn;
+const mapStatetoProps = (state) => ({
+  auth: state.auth,
+});
+
+export default connect(mapStatetoProps, { login })(SignIn);

21a551b84472a8a0597ee299842a80e7051e6b50
Elyes-Ferjani <officialef@hotmail.com>
2020-11-19 23:35:09 +0100

    Styling The App

--- scanner/src/App.js
+++ scanner/src/App.js
@@ -4,21 +4,42 @@ import { BrowserRouter as Router, Link, Route } from "react-router-dom";
 import store from "./store.js";
 import { Provider } from "react-redux";
 import './App.css'
-function App() {
+import React, {Component} from 'react'
+class App extends Component {
+  constructor(props){
+    super(props);
+      this.state={
+        toggle: false
+      }
+  }
+  changeView(){
+    this.setState({toggle: true})
+  }
+  render(){
   return (
     <div className='main'>
       <nav className="navBar"> 
       <div className="icon">
-          <div className='line1'></div>
-          <div className='line2'></div>
-          <div className='line3'></div>
+          <h1 className="title">Qr-</h1>
+          <h1 className="title2">Scanner</h1>
+          
       </div>
         </nav>
+       {!this.state.toggle && <div className="reminder">
+          <center><h1>
+            Reminder
+          </h1>
+          <p>
+            This app is mainly for Qr codes scanning. Please leave the app if you don't have a permission to use it.
+          </p>
+          <p>And if you have a licence from the admin you can</p>
+          <h1  onClick={()=>{this.changeView()}} className='loginText'>Log in</h1>  </center>
+        </div>  }
       <div className='logIn'>
        <center> <Provider store={store}>
           <Router>
             <root>
-              <Route path="/" exact={true} component={SignIn} />
+             {this.state.toggle && <Route path="/" exact={true} component={SignIn} />}
               <Route path="/scanner" component={Scanner} />
             </root>
           </Router>
@@ -26,6 +47,7 @@ function App() {
       </div>
     </div>
   );
+}
 }
 
 export default App;
